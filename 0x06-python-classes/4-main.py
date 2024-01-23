29b4fa2ec7d3df423b927f84cb0b7c99dd8154b1
oladeji21 <oladejiajayi05@gmail.com>
2024-01-23 14:18:18 -0800

    num 4

--- 0x06-python-classes/4-main.py
+++ 0x06-python-classes/4-main.py
@@ -7,7 +7,6 @@ print("Area: {} for size: {}".format(my_square.area(), my_square.size))
 my_square.size = 3
 print("Area: {} for size: {}".format(my_square.area(), my_square.size))
 
-
 try:
     my_square.size = "5 feet"
     print("Area: {} for size: {}".format(my_square.area(), my_square.size))
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" Module of Class Square"""
+
+"""Define a class Square."""
 
 
 class Square:
-    """Blueprint of a square objct"""
+    """Represent a square."""
 
     def __init__(self, size=0):
         """Initialize a new square.
@@ -15,5 +16,17 @@ class Square:
 
     @property
     def size(self):
-        """Gets the current size of the square."""
+        """Get/set the current size of the square."""
         return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
