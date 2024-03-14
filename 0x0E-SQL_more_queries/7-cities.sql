3152e5fa7a3dfda1656233fffaf086145221ed37
oladeji21 <oladejiajayi05@gmail.com>
2024-03-13 23:35:41 -0700

    no 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
--- Script that creates the database hbtn_0d_usa and the table cities
-CREATE DATABASE IF NOT EXIST `hbtn_0d_usa`;
-USE `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `cities`(
-	`id` INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-	`state_id` INT NOT NULL,
-	`name` VARCHAR(256) NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
-);
+-- Script that creates a database and a table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; -- Query to create database
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities ( -- Query to create cities in hbtn_0d_usa
+       id INT UNIQUE AUTO_INCREMENT NOT NULL,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY (id),
+       FOREIGN KEY (state_id) REFERENCES states(id));
