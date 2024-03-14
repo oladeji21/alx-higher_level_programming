190fa01f1811e5dd22bcbee64e27ee7d584de757
oladeji21 <oladejiajayi05@gmail.com>
2024-03-13 23:32:27 -0700

    no 15 and 16

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,9 @@
--- lists all comedy shows in database
-SELECT tv_shows.title
+-- Script that lists all comedy shows only
+SELECT tv_shows.title -- Query to join tables
 FROM tv_shows
-LEFT JOIN tv_genres
-	ON tv_genres.id = tv_show_genres.genre_id
-	INNER JOIN tv_shows
-		ON tv_shows.id = tv_show_genres.show_id
-		WHERE tv_genres.name = 'Comedy';
+     JOIN tv_show_genres
+     	  ON tv_show_genres.show_id = tv_shows.id
+     JOIN tv_genres
+     	  ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
---  lists all shows contained in the database hbtn_0d_tvshows
-SELECT tv_shows.title, tv_genres.name
+-- Script that lists all shows and all genres
+SELECT tv_shows.title, tv_genres.name -- Query to join tables
 FROM tv_shows
-LEFT JOIN tv_show_genres
-	ON tv_shows.id = tv_show_genres.show_id
-	LEFT JOIN tv_show_genres
-		ON tv_genres.id = tv_show_genres.genre_id
-		ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+     LEFT JOIN tv_show_genres
+     	  ON tv_show_genres.show_id = tv_shows.id
+     LEFT JOIN tv_genres
+     	  ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;
