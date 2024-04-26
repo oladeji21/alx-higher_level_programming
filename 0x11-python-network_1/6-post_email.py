d60ecee7a3cd8fb2de560dd4c87a61bec8d2dd83
oladeji21 <oladejiajayi05@gmail.com>
2024-04-26 14:28:05 -0700

    tasks 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""
-Module containing python script that posts a request to a server
-"""
+"""Script that sends a POST request to a given URL with a given email"""
+import sys
 import requests
-from sys import argv
 
-if __name__ = "__main__":
-    email = {'email': argv[2]}
-    url = argv[1]
-    res = requests.post(url, data=email)
-    print(res.text)
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
